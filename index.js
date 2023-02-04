5e2b3f436f1c25cee72d24c6e46193e9812b87fa
Manish0746 <manishgupta0746@gmail.com>
2023-02-04 22:53:44 +0530

    download btn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// menu toggle bar 
 const mobile_menu_btn = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
@@ -5,3 +6,37 @@ function togglenavbar(){
     nav_header.classList.toggle('active')
 }
 mobile_menu_btn.addEventListener("click",() => togglenavbar());
+
+// click to copy 
+const copyMailId = document.querySelectorAll('.mail-logo');
+
+copyMailId.forEach(copyText => {
+    copyText.addEventListener('click', () => {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(copyText);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        try {
+            document.execCommand('copy');
+            selection.removeAllRanges();
+
+            const mailId = copyText.textContent;
+            copyText.textContent = 'Copied!';
+            copyText.classList.add('success');
+
+            setTimeout(() => {
+                copyText.textContent = mailId;
+                copyText.classList.remove('success');
+            }, 1000);
+        } catch (e) {
+            copyText.textContent = 'Couldn\'t copy, hit Ctrl+C!';
+            copyText.classList.add('error');
+
+            setTimeout(() => {
+                errorMsg.classList.remove('show');
+            }, 1200);
+        }
+    });
+});
